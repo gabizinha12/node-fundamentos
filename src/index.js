c9c538269e4d60d3c25c4dc6b114c60a05198284
gabizinha12 <gabrielle.plima1965@gmail.com>
2021-03-15 12:04:19 -0300

    chapter I fundamentos

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const { request, response } = require('express');
+const express = require('express')
+
+const app = express();
+
+
+app.use(express.json())
+
+app.get('/courses', (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3"
+    ])
+})
+
+app.post('/courses', (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+
+app.put('/courses/:id', (request, response) => {
+    const { id } = request.params;
+    console.log(id)
+    return response.json([
+        "Curso 6",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+app.patch('/courses/:id', (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 7",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+app.delete('/courses/:id', (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 2",
+        "Curso 4"
+    ])
+})
+
+app.listen(3333)
